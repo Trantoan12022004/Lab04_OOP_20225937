01571f4dde74fffd3257ac4c34d790cda9933604
Tran_Van_Toan <tranvantoan.work@gmail.com>
2024-12-04 23:52:44 +0700

    sua ten

--- AimsProject/src/hust/soict/dsai/aims/Aims.java
+++ AimsProject/src/hust/soict/dsai/aims/Aims.java
@@ -1,6 +1,6 @@
 package hust.soict.cybersec.aims;
 import hust.soict.cybersec.aims.cart.Cart;
-import hust.soict.cybersec.aims.disc.DigitalVideoDisc;
+import hust.soict.cybersec.aims.media.DigitalVideoDisc;
 
 public class Aims {
  
@@ -9,13 +9,13 @@ public class Aims {
         Cart anOder = new Cart();
         
         DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f); 
-        anOder.addDigitalVideoDisc(dvd1);
+        anOder.addMedia(dvd1);
 
         DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star War", "Science Fiction", "George Lucas", 87, 25.95f); 
-        anOder.addDigitalVideoDisc(dvd2);
+        anOder.addMedia(dvd2);
 
         DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", "Animation", 18.99f);
-        anOder.addDigitalVideoDisc(dvd3);
+        anOder.addMedia(dvd3);
 
         System.out.println("Total cost is: ");
         System.out.println(anOder.totalCost());
--- AimsProject/src/hust/soict/dsai/aims/media/Book.java
+++ AimsProject/src/hust/soict/dsai/aims/media/Book.java
@@ -2,55 +2,22 @@ package hust.soict.cybersec.aims.media;
 
 import java.util.*;
 
-public class Book {
+public class Book extends Media {
     
-    private int id;
-    private String title;
-    private String category;
-    private float cost;
     private List<String> authors = new ArrayList<String>();
-
-    public int getId() {
-        return id;
-    }
-    public String getTitle() {
-        return title;
-    }
-    public String getCategory() {
-        return category;
-    }
-    public float getCost() {
-        return cost;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-    public void setTitle(String title) {
-        this.title = title;
-    }
-    public void setCategory(String category) {
-        this.category = category;
-    }
-    public void setCost(float cost) {
-        this.cost = cost;
-    }
-
-
+    
+    // Constructor 
     public Book(String title) {
-        this.title = title;
+        super(title);
     }
     public Book(String title, String category) {
-        this.title = title;
-        this.category = category;
+        super(title, category);
     }
     public Book(String title, String category, float cost) {
-        this.title = title;
-        this.category = category;
-        this.cost = cost;
+        super(title, category, cost);
     }
 
-
+    // Add and remove author 
     public void addAuthor(String authorName) {
         if (!authors.contains(authorName)) {
             authors.add(authorName);
